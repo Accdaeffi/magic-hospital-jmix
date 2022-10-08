b3e6c07107e676493c1f01a1b219b652b6b52a21
Дмитрий Носков <noskoff2@yandex.ru>
2022-10-08 23:39:54 +0300

    fixed burying patients

--- src/main/java/ru/itmo/mpi/hospital/screen/patient/PatientAdministratorDiseasedBrowse.java
+++ src/main/java/ru/itmo/mpi/hospital/screen/patient/PatientAdministratorDiseasedBrowse.java
@@ -1,10 +1,16 @@
 package ru.itmo.mpi.hospital.screen.patient;
 
+import io.jmix.core.DataManager;
 import io.jmix.ui.action.Action;
 import io.jmix.ui.component.Button;
 import io.jmix.ui.model.CollectionContainer;
 import io.jmix.ui.model.InstanceContainer;
-import io.jmix.ui.screen.*;
+import io.jmix.ui.screen.LookupComponent;
+import io.jmix.ui.screen.StandardLookup;
+import io.jmix.ui.screen.Subscribe;
+import io.jmix.ui.screen.Target;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.itmo.mpi.hospital.entity.Patient;
 import ru.itmo.mpi.hospital.entity.PatientState;
@@ -17,6 +23,9 @@ import java.util.stream.Collectors;
 @UiDescriptor("patient-administrator-diseased-browse.xml")
 @LookupComponent("patientsTable")
 public class PatientAdministratorDiseasedBrowse extends StandardLookup<Patient> {
+
+    @Autowired
+    DataManager dataManager;
 
     @Autowired
     private Button buryBtn;
@@ -38,15 +47,27 @@ public class PatientAdministratorDiseasedBrowse extends StandardLookup<Patient> 
         System.out.println(corpse.toString());
 
         corpse.setPatientState(PatientState.BURIED);
+
+        dataManager.save(corpse);
     }
 
     @Subscribe("patientsTable.buryAll")
     public void onPatientsTableBuryAll(Action.ActionPerformedEvent event) {
         List<Patient> corpses = patientsDc.getItems();
 
-        String result = corpses.stream().filter(c -> c.getPatientState() == PatientState.DISEASED).map(Patient::toString).collect(Collectors.joining("\n"));
+        String result = corpses
+                .stream()
+                .filter(c -> c.getPatientState() == PatientState.DISEASED)
+                .map(Patient::toString)
+                .collect(Collectors.joining("\n"));
         System.out.println(result);
 
-        corpses.stream().filter(c -> c.getPatientState() == PatientState.DISEASED).forEach(c -> c.setPatientState(PatientState.BURIED));
+        corpses.stream()
+                .filter(c -> c.getPatientState() == PatientState.DISEASED)
+                .forEach(c -> {
+                    c.setPatientState(PatientState.BURIED);
+                    dataManager.save(c);
+                });
+
     }
 }
